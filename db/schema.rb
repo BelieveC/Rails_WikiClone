165cd159458c5ecbd9e8cd9684cc01a76b900c42
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-25 12:06:56 +0530

    Add category_id to Article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325060056) do
+ActiveRecord::Schema.define(version: 20160325062612) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
